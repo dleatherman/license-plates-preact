9be31f60f11ac3ce7a3a4ba83540b300d87799da
Dan Leatherman <dleatherman@gmail.com>
2023-03-26 21:41:49 -0400

    logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,14 @@
 import { h } from 'preact';
+
+import Logo from '../logo'
+
 import style from './style.css';
 
 const Header = () => (
 	<header>
 		<a href="/">
-			<h1>License Plates</h1>
+			<h1 class="visually-hidden">License Plates</h1>
+			<Logo aria-hidden />
 		</a>
 	</header>
 );
